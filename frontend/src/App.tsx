c8c6dbfc6e6144662eef6a8de7d9436b8de5626e
Amber-Robeck <94136164+Amber-Robeck@users.noreply.github.com>
2022-05-26 22:22:33 -0500

    Changed minor stylings in App.css and UserInput/styles.css, added task state and passed props to UserInput to collect value

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import UserInput from './components/UserInput/UserInput';
 
 const App: React.FC = () => {
+  const [task, setTask] = useState<string>("")
+  console.log(task)
   return (
     <div className="App">
       <header className="header">Always Be Coding</header>
-      <UserInput />
+      <UserInput task={task} setTask={setTask} />
     </div>
   );
 };
--- frontend/src/components/UserInput/UserInput.tsx
+++ frontend/src/components/UserInput/UserInput.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import "./styles.css"
 
-const UserInput = () => {
+interface Props {
+    task: string;
+    setTask: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const UserInput: React.FC<Props> = ({ task, setTask }) => {
     return (
         <form className='userInput'>
-            <input type="input" placeholder="What are you going to create?" className="userInput__input" />
+            <input type="input" value={task} onChange={(e) => setTask(e.target.value)} placeholder="What are you going to create?" className="userInput__input" />
             <button className="input__submit" type="submit">Get coding</button>
         </form>
     )
