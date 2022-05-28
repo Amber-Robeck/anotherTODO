2c4a585bc78f7fe2d4b920ea12314b41a6ef7ad7
Amber-Robeck <94136164+Amber-Robeck@users.noreply.github.com>
2022-05-27 21:40:25 -0500

    Added handleTask function and  tasks state to save tasks into array on form submit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,27 @@
 import React, { useState } from 'react';
 import './App.css';
 import UserInput from './components/UserInput/UserInput';
+import { Task } from './components/Task';
 
 const App: React.FC = () => {
-  const [task, setTask] = useState<string>("")
-  console.log(task)
+
+  const [task, setTask] = useState<string>("");
+  const [tasks, setTasks] = useState<Task[]>([]);
+
+  const handleTask = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (task) {
+      setTasks([...tasks, { id: Date.now(), task, isCompleted: false }]);
+      setTask("");
+    }
+  };
+
+  // console.log(task)
+  // console.log(tasks)
   return (
     <div className="App">
       <header className="header">Always Be Coding</header>
-      <UserInput task={task} setTask={setTask} />
+      <UserInput task={task} setTask={setTask} handleTask={handleTask} />
     </div>
   );
 };
--- frontend/src/components/UserInput/UserInput.tsx
+++ frontend/src/components/UserInput/UserInput.tsx
@@ -4,11 +4,12 @@ import "./styles.css"
 interface Props {
     task: string;
     setTask: React.Dispatch<React.SetStateAction<string>>;
+    handleTask: (e: React.FormEvent) => void;
 }
 
-const UserInput: React.FC<Props> = ({ task, setTask }) => {
+const UserInput: React.FC<Props> = ({ task, setTask, handleTask }) => {
     return (
-        <form className='userInput'>
+        <form className='userInput' onSubmit={handleTask}>
             <input type="input" value={task} onChange={(e) => setTask(e.target.value)} placeholder="What are you going to create?" className="userInput__input" />
             <button className="input__submit" type="submit">Get coding</button>
         </form>
