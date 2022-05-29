7ec33549f26e2d1e6e46dd669c53975ec23a6350
Amber-Robeck <94136164+Amber-Robeck@users.noreply.github.com>
2022-05-29 16:05:33 -0500

    added function to drag and drop.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,8 @@ import UserInput from './components/UserInput/UserInput';
 import { Task } from './components/Task';
 import TaskList from './components/TaskList/TaskList';
 import { DragDropContext, DropResult } from 'react-beautiful-dnd'
+//leaving here for reference, installing packages left error of 'npm err peer react@ ...
+// npm install your-packages --legacy-peer-deps
 
 const App: React.FC = () => {
 
@@ -20,10 +22,35 @@ const App: React.FC = () => {
   };
 
   const onDragEnd = (result: DropResult) => {
-    console.log(result)
+    // console.log(result)
+    const { source, destination } = result;
+    //if dropped out of droppable
+    if (!destination) return;
+    //if dropped at the same location
+    if (destination.droppableId === source.droppableId && destination.index === source.index) return;
+
+    let move, active = tasks, complete = completedTask;
+
+    //if droppable 1 move to active else move to completed list '2'
+    if (source.droppableId === '1') {
+      move = active[source.index];
+      active.splice(source.index, 1);
+    } else {
+      move = complete[source.index];
+      complete.splice(source.index, 1);
+    }
+
+    if (destination.droppableId === '1') {
+      active.splice(destination.index, 0, move);
+    } else {
+      complete.splice(destination.index, 0, move);
+    }
+
+    setCompletedTask(complete);
+    setTasks(active);
   };
   // console.log(task)
-  console.log(tasks)
+  // console.log(tasks)
   return (
     <DragDropContext onDragEnd={onDragEnd}>
       <div className="App">
