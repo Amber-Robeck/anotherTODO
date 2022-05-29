b521924590f6bf2e8b952693a501b173328c9721
Amber-Robeck <94136164+Amber-Robeck@users.noreply.github.com>
2022-05-29 11:33:34 -0500

    Added more stylings to Onetask css and added a handleComplete function for striking out task

--- frontend/src/components/OneTask/OneTask.tsx
+++ frontend/src/components/OneTask/OneTask.tsx
@@ -7,17 +7,34 @@ interface Props {
     task: Task;
     tasks: Task[];
     setTasks: React.Dispatch<React.SetStateAction<Task[]>>;
-}
+};
 
 const OneTask: React.FC<Props> = ({ task, tasks, setTasks }) => {
+
+    const handleCompleted = (id: number) => {
+        setTasks(
+            tasks.map((task) =>
+                task.id === id ? { ...task, isCompleted: !task.isCompleted } : task
+            )
+        );
+    };
+
     return (
         <form className="task__single ">
-            <span className="task__single--text">{task.task}</span>
+            {
+                task.isCompleted ? (
+                    <s className="task__single--text">{task.task}</s>
+
+                ) : (
+
+                    <span className="task__single--text">{task.task}</span>
+                )
+            }
 
             <div>
                 <span className="icon"><GrEdit /></span>
-                <span className="icon"><GrCheckmark /></span>
-                <span className="icon"><GrTrash /></span>
+                <span className="icon"><GrCheckmark onClick={() => handleCompleted(task.id)} /></span>
+                <span className="icon" ><GrTrash /></span>
             </div>
         </form>
     )
